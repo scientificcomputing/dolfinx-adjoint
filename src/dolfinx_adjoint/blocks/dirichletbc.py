bf7e5de5c887ce1e0e9a54d9c428b68e4962cc05
Henrik Finsberg <henriknf@simula.no>
2026-08-04 14:48:30 +0200

    Make private properties in DirichletBlock

--- src/dolfinx_adjoint/blocks/dirichletbc.py
+++ src/dolfinx_adjoint/blocks/dirichletbc.py
@@ -5,6 +5,16 @@ from pyadjoint.block import Block
 
 
 class DirichletBCBlock(Block):
+    """A block representing a DirichletBC in the adjoint framework.
+
+    Args:
+        value: The value of the Dirichlet BC.
+        dofs: An array of degree-of-freedom indices in `V` where the BC should be applied.
+        V: The function space associated with the Dirichlet BC.
+        ad_block_tag: An optional tag to identify this block in the adjoint framework.
+
+    """
+
     def __init__(
         self,
         value: dolfinx.fem.Function | dolfinx.fem.Constant,
@@ -13,9 +23,17 @@ class DirichletBCBlock(Block):
         ad_block_tag=None,
     ):
         super().__init__(ad_block_tag=ad_block_tag)
-        self.dofs = dofs
-        self.V = V
+        self._dofs = dofs
+        self._V = V
         self.add_dependency(value)
+
+    @property
+    def dofs(self):
+        return self._dofs
+
+    @property
+    def V(self):
+        return self._V
 
     def prepare_recompute_component(self, inputs, relevant_outputs):
         return inputs[0] if inputs else None
