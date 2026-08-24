4e7fa46e5e8dd8b3baa5d98c2a4b891d45b8597d
Joergen Schartum Dokken <dokken@simula.no>
2026-08-24 11:45:08 +0000

    Fix mypy

--- src/dolfinx_adjoint/compat.py
+++ src/dolfinx_adjoint/compat.py
@@ -4,6 +4,6 @@ import dolfinx
 def get_interpolation_points(V: dolfinx.fem.FunctionSpace):
     """Get the interpolation points for a given function space V."""
     try:
-        return V.element.interpolation_points()
+        return V.element.interpolation_points()  # type: ignore[operator]
     except TypeError:
         return V.element.interpolation_points
